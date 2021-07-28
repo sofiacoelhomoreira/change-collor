7c6247b1293abc6f825251cfe49dff38dc886320
sofiacoelhomoreira <scmsofia@gmail.com>
2021-07-28 14:21:40 -0300

    using java script and button to change collor

---
+++ src/change.js
@@ -0,0 +1,12 @@
+
+document.getElementById("btn-calm").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "calm");
+});
+
+document.getElementById("btn-mystic").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "mystic");
+});
+
+document.getElementById("btn-creative").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "creative");
+});
